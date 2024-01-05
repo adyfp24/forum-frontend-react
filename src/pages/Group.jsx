facc1978c4eb6407faf798884f2736f9daa04272
adyfp24 <adyfp24@gmail.com>
2024-01-05 10:43:43 +0700

    styling page groupchat dan prodile group

--- src/pages/Group.jsx
+++ src/pages/Group.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navbar from '../includes/Navbar';
 import Sidebar from '../includes/Sidebar';
+import profileUIUX from '../assets/img/11.png';
 
 function Group() {
   return (
@@ -9,107 +10,219 @@ function Group() {
         <Sidebar />
         <div className="flex flex-col w-full text-gray-800">
           <Navbar />
-          <div className='flex w-full h-screen'>
-           
-                <div class="flex flex-row h-full w-full  ">
-                  <div class="flex flex-col p-6 flex-auto h-full overflow-y-auto"> 
+          <div className='flex w-full overflow-y-auto'>
 
-                      <div className='flex flex-col items-start h-full mb-6 rounded-xl  bg-white w-full px-4'>
-                          divv
-                      </div>
-                      <div className='flex flex-col items-start h-full mb-6 rounded-xl  bg-white w-full px-4'>
-                          divv
-                      </div>
-                      <div className='flex flex-col items-start h-full mb-6 rounded-xl  bg-white w-full px-4'>
-                          divv
-                      </div>
-                      <div className='flex flex-col items-start h-full mb-6 rounded-xl  bg-white w-full px-4'>
-                          divv
-                      </div>
-                      <div className='flex flex-col items-start h-full mb-6 rounded-xl bg-white w-full px-4'>
-                          divv
-                      </div>
-                      <div className='flex flex-col items-start h-full mb-6 rounded-xl bg-white w-full px-4'>
-                          divv
-                      </div>
-                      <div className='flex flex-col items-start h-full mb-6 rounded-xl  bg-white w-full px-4'>
-                          divv
-                      </div>
-                      <div className='flex flex-col items-start h-full mb-6 rounded-xl  bg-white w-full px-4'>
-                          divv
-                      </div>
-                      <div className='flex flex-col items-start h-full mb-6 rounded-xl  bg-white w-full px-4'>
-                          divv
-                      </div>
-                      <div className='flex flex-col items-start h-full mb-6 rounded-xl  bg-white w-full px-4'>
-                          divv
-                      </div>
-                      <div className='flex flex-col items-start h-full mb-6 rounded-xl  bg-white w-full px-4'>
-                          divv
-                      </div>
-                    
-
-                      <div
-                        class="flex flex-row items-center h-16 mb-0 rounded-xl bg-white w-full px-4"
-                      >
-                        <div>
-                          <button
-                            class="flex items-center justify-center text-gray-400 hover:text-gray-600"
+            <div class="flex flex-col flex-auto h-full p-6 w-3/4">
+              <div
+                class="flex flex-col flex-auto flex-shrink-0 rounded-2xl bg-gray-100 h-full p-4"
+              >
+                <div class="flex flex-col h-full overflow-x-auto mb-4">
+                  <div class="flex flex-col h-full">
+                    <div class="grid grid-cols-12 gap-y-2">
+                      <div class="col-start-1 col-end-8 p-3 rounded-lg">
+                        <div class="flex flex-row items-center">
+                          <div
+                            class="flex items-center justify-center h-10 w-10 rounded-full bg-indigo-500 flex-shrink-0"
                           >
-                            <svg
-                              class="w-5 h-5"
-                              fill="none"
-                              stroke="currentColor"
-                              viewBox="0 0 24 24"
-                              xmlns="http://www.w3.org/2000/svg"
-                            >
-                              <path
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
-                                stroke-width="2"
-                                d="M15.172 7l-6.586 6.586a2 2 0 102.828 2.828l6.414-6.586a4 4 0 00-5.656-5.656l-6.415 6.585a6 6 0 108.486 8.486L20.5 13"
-                              ></path>
-                            </svg>
-                          </button>
-                        </div>
-                        <div class="flex-grow ml-4">
-                          <div class="relative w-full">
-                            <input
-                              type="text"
-                              class="flex w-full border rounded-xl focus:outline-none focus:border-indigo-300 pl-4 h-10"
-                            />
+                            A
+                          </div>
+                          <div
+                            class="relative ml-3 text-sm bg-white py-2 px-4 shadow rounded-xl"
+                          >
+                            <div>Hey How are you today?</div>
                           </div>
                         </div>
-                        <div class="ml-4">
-                          <button
-                            class="flex items-center justify-center bg-indigo-500 hover:bg-indigo-600 rounded-xl text-white px-4 py-1 flex-shrink-0"
+                      </div>
+                      <div class="col-start-1 col-end-8 p-3 rounded-lg">
+                        <div class="flex flex-row items-center">
+                          <div
+                            class="flex items-center justify-center h-10 w-10 rounded-full bg-indigo-500 flex-shrink-0"
                           >
-                            <span>Send</span>
-                            <span class="ml-2">
-                              <svg
-                                class="w-4 h-4 transform rotate-45 -mt-px"
-                                fill="none"
-                                stroke="currentColor"
-                                viewBox="0 0 24 24"
-                                xmlns="http://www.w3.org/2000/svg"
-                              >
-                                <path
-                                  stroke-linecap="round"
-                                  stroke-linejoin="round"
-                                  stroke-width="2"
-                                  d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8"
-                                ></path>
-                              </svg>
-                            </span>
-                          </button>
+                            A
+                          </div>
+                          <div
+                            class="relative ml-3 text-sm bg-white py-2 px-4 shadow rounded-xl"
+                          >
+                            <div>
+                              Lorem ipsum dolor sit amet, consectetur adipisicing
+                              elit. Vel ipsa commodi illum saepe numquam maxime
+                              asperiores voluptate sit, minima perspiciatis.
+                            </div>
+                          </div>
                         </div>
                       </div>
-
-
+                      <div class="col-start-6 col-end-13 p-3 rounded-lg">
+                        <div class="flex items-center justify-start flex-row-reverse">
+                          <div
+                            class="flex items-center justify-center h-10 w-10 rounded-full bg-indigo-500 flex-shrink-0"
+                          >
+                            A
+                          </div>
+                          <div
+                            class="relative mr-3 text-sm bg-indigo-100 py-2 px-4 shadow rounded-xl"
+                          >
+                            <div>I'm ok what about you?</div>
+                          </div>
+                        </div>
+                      </div>
+                      <div class="col-start-6 col-end-13 p-3 rounded-lg">
+                        <div class="flex items-center justify-start flex-row-reverse">
+                          <div
+                            class="flex items-center justify-center h-10 w-10 rounded-full bg-indigo-500 flex-shrink-0"
+                          >
+                            A
+                          </div>
+                          <div
+                            class="relative mr-3 text-sm bg-indigo-100 py-2 px-4 shadow rounded-xl"
+                          >
+                            <div>
+                              Lorem ipsum dolor sit, amet consectetur adipisicing. ?
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                      <div class="col-start-1 col-end-8 p-3 rounded-lg">
+                        <div class="flex flex-row items-center">
+                          <div
+                            class="flex items-center justify-center h-10 w-10 rounded-full bg-indigo-500 flex-shrink-0"
+                          >
+                            A
+                          </div>
+                          <div
+                            class="relative ml-3 text-sm bg-white py-2 px-4 shadow rounded-xl"
+                          >
+                            <div>Lorem ipsum dolor sit amet !</div>
+                          </div>
+                        </div>
+                      </div>
+                      <div class="col-start-6 col-end-13 p-3 rounded-lg">
+                        <div class="flex items-center justify-start flex-row-reverse">
+                          <div
+                            class="flex items-center justify-center h-10 w-10 rounded-full bg-indigo-500 flex-shrink-0"
+                          >
+                            A
+                          </div>
+                          <div
+                            class="relative mr-3 text-sm bg-indigo-100 py-2 px-4 shadow rounded-xl"
+                          >
+                            <div>
+                              Lorem ipsum dolor sit, amet consectetur adipisicing. ?
+                            </div>
+                            <div
+                              class="absolute text-xs bottom-0 right-0 -mb-5 mr-2 text-gray-500"
+                            >
+                              Seen
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                      <div class="col-start-1 col-end-8 p-3 rounded-lg">
+                        <div class="flex flex-row items-center">
+                          <div
+                            class="flex items-center justify-center h-10 w-10 rounded-full bg-indigo-500 flex-shrink-0"
+                          >
+                            A
+                          </div>
+                          <div
+                            class="relative ml-3 text-sm bg-white py-2 px-4 shadow rounded-xl"
+                          >
+                            <div>
+                              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                              Perspiciatis, in.
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
                   </div>
                 </div>
+                <div
+                  class="flex flex-row items-center h-16 rounded-xl bg-white w-full px-4"
+                >
+                  <div>
+                    <button
+                      class="flex items-center justify-center text-gray-400 hover:text-gray-600"
+                    >
+                      <svg
+                        class="w-5 h-5"
+                        fill="none"
+                        stroke="currentColor"
+                        viewBox="0 0 24 24"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          stroke-width="2"
+                          d="M15.172 7l-6.586 6.586a2 2 0 102.828 2.828l6.414-6.586a4 4 0 00-5.656-5.656l-6.415 6.585a6 6 0 108.486 8.486L20.5 13"
+                        ></path>
+                      </svg>
+                    </button>
+                  </div>
+                  <div class="flex-grow ml-4">
+                    <div class="relative w-full">
+                      <input
+                        type="text"
+                        class="flex w-full border rounded-xl focus:outline-none focus:border-indigo-300 pl-4 h-10"
+                      />
+                      <button
+                        class="absolute flex items-center justify-center h-full w-12 right-0 top-0 text-gray-400 hover:text-gray-600"
+                      >
+                        <svg
+                          class="w-6 h-6"
+                          fill="none"
+                          stroke="currentColor"
+                          viewBox="0 0 24 24"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            stroke-width="2"
+                            d="M14.828 14.828a4 4 0 01-5.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                          ></path>
+                        </svg>
+                      </button>
+                    </div>
+                  </div>
+                  <div class="ml-4">
+                    <button
+                      class="flex items-center justify-center bg-indigo-500 hover:bg-indigo-600 rounded-xl text-white px-4 py-1 flex-shrink-0"
+                    >
+                      <span>Send</span>
+                      <span class="ml-2">
+                        <svg
+                          class="w-4 h-4 transform rotate-45 -mt-px"
+                          fill="none"
+                          stroke="currentColor"
+                          viewBox="0 0 24 24"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            stroke-width="2"
+                            d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8"
+                          ></path>
+                        </svg>
+                      </span>
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
 
-            
+            <div className='flex flex-col justify-center w-72 items-center bg-gray-800'>
+              <a className="inline-flex w-full items-center justify-center mt-8">
+                <h1 className='text-white font-semibold text-3xl'>Forum UI/UX</h1>
+              </a>
+              <div className='mt-8 mb-4  flex justify-center rounded-full w-40 h-40 bg-gray-100'>
+                <img className='rounded-full w-full h-full object-cover' src={profileUIUX} alt="" />
+              </div>
+              <a className="items-center justify-center my-6 mx-6">
+                <h4 className='text-white font-normal text-justify text-xs'>sarana diskusi bagi mahasiswa ilkom yang memiliki minat pada bidang visual desain khususnya UI/UX design, disini mahasiswa dapat saling bertukar ide, style desain, design system, dan belajar satu sama lain</h4>
+              </a>
+            </div>
 
           </div>
         </div>
